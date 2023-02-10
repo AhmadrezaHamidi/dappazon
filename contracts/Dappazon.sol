7db0699e279330582bf381c1099f0c7f1c33a0c8
mahsa <mahsa_karim16@yahoo.com>
2023-02-10 19:04:54 +0330

    added an event

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -4,6 +4,19 @@ pragma solidity ^0.8.9;
 contract Dappazon {
     address public owner;
 
+    struct Item {
+        uint256 id;
+        string name;
+        string category;
+        string image;
+        uint256 cost;
+        uint256 rating;
+        uint256 stock;
+    }
+    mapping(uint256 => Item) public items;
+
+    event List(string name, uint256 cost, uint256 quantity);
+
     constructor() {
         owner = msg.sender;
     }
@@ -17,7 +30,20 @@ contract Dappazon {
         uint256 _cost,
         uint256 _rating,
         uint256 _stock
-    ) public {}
+    ) public {
+        Item memory item = Item(
+            _id,
+            _name,
+            _category,
+            _image,
+            _cost,
+            _rating,
+            _stock
+        );
+        items[_id] = item;
+
+        emit List(_name, _cost, _stock);
+    }
 
     //buy products
 
