b6031d09ef85bbe3570a5162167cf3d437f13762
mahsa <mahsa_karim16@yahoo.com>
2023-02-11 15:03:23 +0330

    added test

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -13,9 +13,18 @@ contract Dappazon {
         uint256 rating;
         uint256 stock;
     }
+
+    struct Order {
+        uint256 time;
+        Item item;
+    }
+
     mapping(uint256 => Item) public items;
+    mapping(address => uint256) public orderCount;
+    mapping(address => mapping(uint256 => Order)) public orders;
 
     event List(string name, uint256 cost, uint256 quantity);
+    event Buy(address buyer, uint256 orderId, uint256 itemId);
 
     modifier onlyOwner() {
         require(msg.sender == owner);
@@ -51,6 +60,16 @@ contract Dappazon {
     }
 
     //buy products
+    function buy(uint256 _id) public payable {
+        //fetch item
+        Item memory item = items[_id];
+        // create an order
+        Order memory order = Order(block.timestamp, item);
+        // save order to chain
+        orderCount[msg.sender]++;
+        orders[msg.sender][orderCount[msg.sender]] = order;
 
+        items[_id].stock = item.stock - 1;
+    }
     //withdraw funds
 }
